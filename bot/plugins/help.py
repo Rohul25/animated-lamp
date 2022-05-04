1e56ab7ccd7d2c3a33aafdabe5355b1f50383087
Mr.Rohul <92625688+Rohul25@users.noreply.github.com>
2022-05-04 11:33:37 +0530

    Update help.py

--- bot/plugins/help.py
+++ bot/plugins/help.py
@@ -5,25 +5,19 @@ from bot.config import Config
 
 
 HELP_TEXT = """
-Hi {mention}. Welcome to Screenshot Generator Bot. You can use me to generate:
+Hi {mention}. Welcome to Converter Bot Made By @ViralBeatz. 
+You can use me to generate:
 
     1. Screenshots.
     2. Sample Video.
     3. Trim Video.
 
-👉 I support any kind of **telegram video file** (streaming video or document video files) provided it --has proper mime-type-- and --is not corrupted--.
-👉 I also support **Streaming URLs**. The URL should be a --streaming URL--, --non IP specific--, and --should return proper response codes--.
-Just send me the telegram file or the streaming URL.
+Send A Video Select Which You Need To Do By Me. Wait A Few Seconds. 
 
-See /settings to configure bot's behavior.
-Use /set_watermark to set custom watermarks to your screenshots.
+Send /settings For More Info & Choose Which Need. 
 
-**General FAQ.**
 
-👉 If the bot dosen't respond to telegram files you forward, first check /start and --confirm bot is alive--. Then make sure the file is a **video file** which satisfies above mentioned conditions.
-👉 If bot replies __😟 Sorry! I cannot open the file.__, the file might be --currupted-- or --is malformatted--.
-
-__If issues persists contact my father.__
+__If issues persists contact @ViralBeatz.__
 
 {admin_notification}
 """
